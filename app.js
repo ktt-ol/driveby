e62a2a476532f9aa53f10e85fb97cad730d3c8e5
Arne Hude <arne.hude@arnehude.de>
2019-11-02 16:24:38 +0100

    size ajustments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.engine('pug', require('pug').__express)
+app.engine('pug', require('pug').__express);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
