bae22bdf1c174140be0cd52c68e4adae28dd4b1b
Arne Hude <arne.hude@arnehude.de>
2019-11-02 22:07:21 +0100

    added Real Content

--- controllers/fahrplan.js
+++ controllers/fahrplan.js
@@ -6,7 +6,15 @@ module.exports.getJSON = ()=>{
     return new Promise((resolve, reject) => {
         //axios.get("https://departure.oepnvmap.de/HOLD.json?version=3&limit=100")
         axios.get("https://api.oepnvmap.de/olbus.dat")
-        .then(res =>{resolve(res.data)})
-        .catch(err=>reject(err))
+        .then(res =>{resolve(res.data);})
+        .catch(err=>reject(err));
+    });   
+};
+module.exports.getJSONdeparture = (station)=>{
+    return new Promise((resolve, reject) => {
+        //axios.get("https://departure.oepnvmap.de/HOLD.json?version=3&limit=100")
+        axios.get("https://departure.oepnvmap.de/"+station+".json?version=3")
+        .then(res =>{resolve(res.data);})
+        .catch(err=>reject(err));
     });   
 };
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 /*jshint esversion: 6 */
+
+const __DATASOURCE = 2;
+const __FROMSTATION = "Oldenburg(Oldb)";
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -38,7 +43,7 @@ let processPlaintext = (d)=>{
       'realdepature': realDepature,
       'delay': delay,
       'vehicle': i.slice(12,20),
-      'station': i.slice(21,63).split("  ",1)[0].split("(Oldenburg)",1)[0], 
+      'destination': i.slice(21,63).split("  ",1)[0].split("(Oldenburg)",1)[0], 
       'track': i.slice(64,80).split("  ",3)[1] || null,
       'info': i.slice(70,120) || null,
     };
@@ -47,14 +52,80 @@ let processPlaintext = (d)=>{
   return destinations;
 };
 
+
+let processJSON = (d)=>{
+  d = d[0].departures;
+  let tmp = 0;   
+  let destinations ={};
+
+  for(let index in d){
+    if(d[index].destination != __FROMSTATION){
+      
+      let depHr = d[index].scheduledDeparture.split(":",2)[0];
+      depHr = parseInt(depHr);
+
+      let depMin = d[index].scheduledDeparture.split(":",2)[1];
+      depMin = parseInt(depMin);
+
+      let delay = d[index].delayDeparture;
+      if(depMin+delay >= 60){      
+        let overshoot = (depMin+delay)-60;
+        if(overshoot<10){
+          realDepature = (depHr+1)+":0"+(overshoot);
+        }else{
+          realDepature = (depHr+1)+":"+(overshoot);
+        }
+      }else{
+        if(depMin+delay<10){
+          realDepature = depHr+":0"+(depMin+delay);
+        }else{
+          realDepature = depHr+":"+(depMin+delay);
+        }
+      }
+      destinations[tmp] = {
+        'departure': d[index].scheduledDeparture,
+        'realdepature': realDepature,
+        'delay': d[index].delayArrival,
+        'vehicle': d[index].train,
+        'destination': d[index].destination, 
+        'track': d[index].platform,
+        'info': d[index].messages,
+        'isCancelled': d[index].isCancelled,
+        'via': d[index].via,
+      };
+    }
+    tmp++;
+  }
+  //console.log(destinations);
+  return destinations;
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   let schedule = require("../controllers/fahrplan");
-  let destinationData = schedule.getJSON();
-
+  let destinationData = "";
+  switch (__DATASOURCE) {
+    case 1:
+      destinationData = schedule.getJSON();
+      break;
+    case 2:
+      destinationData = schedule.getJSONdeparture('HOLD');
+      break;  
+    default:
+      break;
+  }
   Promise.all([destinationData]).then(d =>{
-    d = processPlaintext(d);
+    switch (__DATASOURCE) {
+      case 1:
+        d = processPlaintext(d);
+        break;
+      case 2:
+        d = processJSON(d);
+        break;
+      default:
+        break;
+    }
     res.render('index', { title: 'Express', data : d});
   });
 });
